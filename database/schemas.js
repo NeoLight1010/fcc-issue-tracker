d8ddfdc0fc9e8e7c874045d7441bfe2764061e6b
Anthony Suárez <neolight1010@gmail.com>
2021-03-18 13:50:20 -0500

    Add createIssue() to db utils

--- database/schemas.js
+++ database/schemas.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const IssueSchema = new mongoose.Schema({
-    issue_title: String,
-    issue_text: String,
-    created_by: String,
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_by: {type: String, required: true},
     assigned_to: String,
     status_text: String,
     open: Boolean,
     created_on: Date,
-    updated_on: Date
+    updated_on: Date,
 });
 
 exports.IssueSchema = IssueSchema;
--- database/utils.js
+++ database/utils.js
@@ -7,4 +7,23 @@ function deleteAllIssues(done) {
   });
 }
 
+function createIssue(done, title, text, createdBy, assignedTo="", statusText="") {
+  const newIssue = new Issue({
+    issue_title: title,
+    issue_text: text,
+    created_by: createdBy,
+    assigned_to: assignedTo,
+    status_text: statusText,
+    open: true,
+    created_on: Date.now(),
+    updated_on: Date.now()
+  });
+
+  newIssue.save((err, data) => {
+    if (err) return console.log(err);
+    done(null, data);
+  });
+}
+
 exports.deleteAllIssues = deleteAllIssues;
+exports.createIssue = createIssue;
